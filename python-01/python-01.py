be4841ff5e65d3789345b54ea9cec2683cb06bf8
sontek <sontek@yeah.net>
2016-07-17 21:38:50 +0800

    add python start

---
+++ python-01/python-01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+# 打印 Hello World
+# python3.x 之后，print 作为一个函数使用，2.x是一个关键字，所以使用函数方式
+
+print("Hello World")
+print("张三")
---
+++ python-01/python-02.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+#  学习Python数据类型
+
+a = 10;
+b = 1.3
+c = True
+d = 'Hi'
+print (a, type(a))
+print (b, type(b))
+print (c, type(c))
+print (d, type(d))
