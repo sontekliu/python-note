399b1b18debaff2ac91f37941e6c27c7a5d26b55
javaliu <sontekliu@outlook.com>
2016-07-19 22:05:13 +0800

    add python-03.py

---
+++ python-01/python-03.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+t1 = (1,2,"a",True,False);
+t2 = [1,2,"a",True,False];
+
+print (t1, type(t1))
+print (t2, type(t2))
+
+print "张三"
+print t1[0:3:2]
+
+print t2[0:5:-1] 
