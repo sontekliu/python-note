0faa4d954d58900b8eb353fe4ff36503eec787b5
javaliu <sontekliu@outlook.com>
2016-07-20 18:31:08 +0800

    add dict study

--- python-01/python-06.py
+++ python-01/python-06.py
@@ -4,13 +4,20 @@
 # dict学习
 # 1. dict 定义
 d = {'micale': 90, 'bob':89, 'javaliu' : 99}
-# 判断是否在Key
-# key in d
+# 2. 判断是否在Key, key in d
 print 'bob' in d
 
-# 取出key对应的值，若不存在返回None，第二个参数可指定不存在时的值
+# 3. 取出key对应的值，若不存在返回None，第二个参数可指定不存在时的值
 print d.get('bob')
 print d.get('bobaaa', -1)
+
+# 4. 显示dict
+print d
+
+# 5. 删除对应的key
+d.pop('bob')
+
+print d
 
 
 
