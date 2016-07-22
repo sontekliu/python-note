8497ee16290011f3c34ee895fbbd52ff82309204
javaliu <sontekliu@outlook.com>
2016-07-22 09:54:12 +0800

    add python function intro

---
+++ python-02/python-01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+# 函数简介 （python中常用函数）
+
+print abs(100)
+print abs(-90)
+
+print cmp(3,4)
+print cmp(3,3)
+print cmp(3,2)
+
+print int('123')
+print int(12.34)
+
+print str(1.23)
+
+a = abs
+print a(-20)
