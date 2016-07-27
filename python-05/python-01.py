46780dff47db8ab0aa88d190b33a36365080769e
javaliu <sontekliu@outlook.com>
2016-07-27 17:06:29 +0800

    add python-01.py

---
+++ python-05/python-01.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+# python中的模块，一个xxx.py文件就是一个模块。
+# 如果模块名字冲突，则使用包的方式来组织模块，就像java里面的package
+# 例如:
+#	mymodel
+#		__init__.py
+#		abc.py
+#		xyz.py
+#
+#   表示有两个模块，分别是 mymodel.abc和mymodel.xyz，每个包目录下面必须有一个__init__.py文件，
+#   否则python就把这个目录当成普通目录，而不是一个包。
+#
+#
+#
