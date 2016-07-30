45c3f539fb6dcffca1fd7bf3172d688fea7215df
sontek <sontek@yeah.net>
2016-07-30 21:15:14 +0800

    add python-04.py

---
+++ python-05/python-04.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+# python 安装第三方模块
+# 1. 首先安装 easy_install
+# wget http://peak.telecommunity.com/disk/ez_setup.py
+# python ez_setup.py
+# 添加环境变量
+# export PYTHON_HOME=/usr/local/python2.7
+# export PATH=...:PYTHON_HOME/bin:$PATH
+
+# 2.0 安装pip
+# easy_install pip
+# 使用pip安装第三方库
+# pip install MySQL-python
+# pip install mysql-connector
+
+
+
+
+
